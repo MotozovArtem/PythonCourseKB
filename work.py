3643558468c2435856e539847b3a5ae08cf54363
Rienel <mav199824@yandex.ru>
2018-03-18 15:56:45 +0300

    Lection 4

---
+++ work.py
@@ -0,0 +1,30 @@
+import random
+
+
+def func(x):
+    if x >= 0:
+        return x ** 2
+    return x
+
+
+if __name__ == '__main__':
+    # Первый вариант (в функциональном стиле)
+    a = list()
+    for i in range(20):
+        a.append(random.randint(-50, 50))
+    print(a)
+    a = list(map(func, a))
+    print(a)
+
+    print()
+    # Второй вариант (в процедурном стиле)
+    a = list()
+    for i in range(20):
+        a.append(random.randint(-50, 50))
+    print(a)
+    for i in range(len(a)):
+        if a[i] >= 0:
+            a[i] **= 2
+            # a[i] = a[i] ** 2 Аналогичная запись
+    print(a)
+
